aae812e58c03fef8c524a1d5919bd159483af4d8
Ryan Krueger <ryankrueger@gmail.com>
2012-05-31 16:40:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RyanSports2::Application.config.secret_token = 'c986f706949e9452233f79a794bbaa53eacefc58d302330c9672ed45a2500ff146b08cf556eb1eaba326ee620273477dbab4d51453441aa2b94ba3714dabc340'
