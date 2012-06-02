8cd337a85cd3aa1cf8fcf4c14e6ad95bd80aaee1
Tom Hoeck <tomhoeck@gmail.com>
2012-06-02 12:27:13 -0700

    devise fix for heroku ??

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module RyanSports2
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #  Using devise on Heroku with Ruby on Rails 3.1 requires setting:
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
